84fcd677562d5d851c072fb090c79253564213be
Andre Baptista <albs_br@yahoo.com.br>
2021-12-07 22:18:28 -0300

    v.0.9.0

--- Sprites/LoadSprites.s
+++ Sprites/LoadSprites.s
@@ -4,61 +4,183 @@ LoadSprites:
 
     ; --- Sprite patterns
 
+
+    ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right
+    ld      IX, SPRPAT
+    ld      IY, SPRCOL
+    ld      b, 4
+    call    LoadSpritesWithColors
+
     ; Spr 0 pattern
-    ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 0)
-    ld      de, SPRPAT + (32 * 0)
-    call    LoadSpritePatterns_16x16
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 0)
+    ; ld      de, SPRPAT + (32 * 0)
+    ; call    LoadSpritePatterns_16x16
 
-    ; Spr 1 pattern
-    ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 1)
-    ld      de, SPRPAT + (32 * 1)
-    call    LoadSpritePatterns_16x16
+    ; ; Spr 1 pattern
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 1)
+    ; ld      de, SPRPAT + (32 * 1)
+    ; call    LoadSpritePatterns_16x16
 
-    ; Spr 2 pattern
-    ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 2)
-    ld      de, SPRPAT + (32 * 2)
-    call    LoadSpritePatterns_16x16
+    ; ; Spr 2 pattern
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 2)
+    ; ld      de, SPRPAT + (32 * 2)
+    ; call    LoadSpritePatterns_16x16
 
-    ; Spr 3 pattern
-    ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 3)
-    ld      de, SPRPAT + (32 * 3)
-    call    LoadSpritePatterns_16x16
+    ; ; Spr 3 pattern
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 3)
+    ; ld      de, SPRPAT + (32 * 3)
+    ; call    LoadSpritePatterns_16x16
 
 
 
     ; --- Sprite colors
 
-    ; Spr 0 color
-    ; ld      a, 0000 0001 b
-    ; ld      hl, SPRCOL + (16 * 0)
-    ; call    SetVdp_Write
-    ; ld      b, 16
-    ; ld      c, PORT_0        ; you can also write ld bc,#nn9B, which is faster
+    ; ; Spr 0 color
     ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 0) + 32
-    ; otir
-    ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 0) + 32
-    ld      de, SPRCOL + (16 * 0)
-    call    LoadSpriteColors_16x16
+    ; ld      de, SPRCOL + (16 * 0)
+    ; call    LoadSpriteColors_16x16
 
-    ; Spr 1 color
-    ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 1) + 32
-    ld      de, SPRCOL + (16 * 1)
-    call    LoadSpriteColors_16x16
+    ; ; Spr 1 color
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 1) + 32
+    ; ld      de, SPRCOL + (16 * 1)
+    ; call    LoadSpriteColors_16x16
 
-    ; Spr 2 color
-    ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 2) + 32
-    ld      de, SPRCOL + (16 * 2)
-    call    LoadSpriteColors_16x16
+    ; ; Spr 2 color
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 2) + 32
+    ; ld      de, SPRCOL + (16 * 2)
+    ; call    LoadSpriteColors_16x16
 
-    ; Spr 3 color
-    ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 3) + 32
-    ld      de, SPRCOL + (16 * 3)
-    call    LoadSpriteColors_16x16
+    ; ; Spr 3 color
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 3) + 32
+    ; ld      de, SPRCOL + (16 * 3)
+    ; call    LoadSpriteColors_16x16
 
     ret
 
 
 
+; HL: source on RAM
+; IX: pattern destiny on VRAM
+; IY: color destiny on VRAM
+; B: number of sprites
+LoadSpritesWithColors:
+
+    ; Spr 0 pattern
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 0)
+    ; ld      de, SPRPAT + (32 * 0)
+
+    push    hl
+        push    bc
+
+.loop:
+            push    bc
+                push    hl
+                    ld      d, ixh
+                    ld      e, ixl
+                    call    LoadSpritePatterns_16x16
+                pop     hl
+                call    .nextHLAndIX
+            pop     bc
+
+            djnz    .loop
+
+        pop     bc
+    pop     hl
+
+
+    ; ; Spr 1 pattern
+    ; ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 1)
+    ; ; ld      de, SPRPAT + (32 * 1)
+    ; call    LoadSpritePatterns_16x16
+
+    ; ; Spr 2 pattern
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 2)
+    ; ld      de, SPRPAT + (32 * 2)
+    ; call    LoadSpritePatterns_16x16
+
+    ; ; Spr 3 pattern
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 3)
+    ; ld      de, SPRPAT + (32 * 3)
+    ; call    LoadSpritePatterns_16x16
+
+
+    ; HL += 32
+    ld      de, 32
+    add     hl, de
+
+.loop1:
+    push    bc
+
+        ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 0) + 32
+        ; ld      de, SPRCOL + (16 * 0)
+        push    hl
+            ld      d, iyh
+            ld      e, iyl
+            call    LoadSpriteColors_16x16
+        pop     hl
+        call    .nextHLAndIY
+
+    pop     bc
+
+    djnz    .loop1
+
+
+
+    ; Spr 0 color
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 0) + 32
+    ; ld      de, SPRCOL + (16 * 0)
+    ; call    LoadSpriteColors_16x16
+
+    ; ; Spr 1 color
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 1) + 32
+    ; ld      de, SPRCOL + (16 * 1)
+    ; call    LoadSpriteColors_16x16
+
+    ; ; Spr 2 color
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 2) + 32
+    ; ld      de, SPRCOL + (16 * 2)
+    ; call    LoadSpriteColors_16x16
+
+    ; ; Spr 3 color
+    ; ld      hl, SpritePatternsAndColors_SantaClaus_Standing_Right + (48 * 3) + 32
+    ; ld      de, SPRCOL + (16 * 3)
+    ; call    LoadSpriteColors_16x16
+
+
+
+    ret
+
+.nextHLAndIX:
+    ; HL += 48
+    ld      bc, 48
+    add     hl, bc
+    ; IX += 32
+    push    hl
+        ld      d, ixh
+        ld      e, ixl
+        ld      hl, 32
+        add     hl, de
+        ex      de, hl
+        ld      ixh, d
+        ld      ixl, e
+    pop     hl
+    ret
+
+.nextHLAndIY:
+    ; HL += 48
+    ld      bc, 48
+    add     hl, bc
+    ; IX += 16
+    push    hl
+        ld      d, iyh
+        ld      e, iyl
+        ld      hl, 16
+        add     hl, de
+        ex      de, hl
+        ld      iyh, d
+        ld      iyl, e
+    pop     hl
+    ret
 
 ; HL: source on RAM
 ; DE: destiny on VRAM
