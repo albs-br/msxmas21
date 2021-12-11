d625d6a318341a72909536abb6908cee6f565884
Andre Baptista <albs_br@yahoo.com.br>
2021-12-11 19:36:39 -0300

    v.0.17.1

--- Variables.s
+++ Variables.s
@@ -5,10 +5,21 @@ PlayerAnimationFrame:       rb 1
 
 
 ; Gift struct
+Gift_1_Struct:
+Gift_1_Status:              rb 1
+Gift_1_ConveyorBeltEnd:     rb 1
 Gift_1_X:                   rb 1
 Gift_1_Y:                   rb 1
 Gift_1_Dx:                  rb 1
 Gift_1_Dy:                  rb 1
+Gift_1_Struct.size:         equ $ - Gift_1_Struct
+
+
+Gift_2_Struct:              rb Gift_1_Struct.size
+Gift_3_Struct:              rb Gift_1_Struct.size
+Gift_4_Struct:              rb Gift_1_Struct.size
+Gift_5_Struct:              rb Gift_1_Struct.size
+Gift_6_Struct:              rb Gift_1_Struct.size
 
 
 
