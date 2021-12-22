82848fef3a4e55dfda6731df55ab1edbcdab5523
Andre Baptista <albs_br@yahoo.com.br>
2021-12-22 00:11:01 -0300

    v.0.35.0

--- InitVram.s
+++ InitVram.s
@@ -189,50 +189,82 @@ Load_SC5_Image:
 ;   HL: source on RAM
 ;   ADE: 17-bits destiny on VRAM
 Load_16x16_SC5_Image_WithTransparency:
-    ld      b, 16               ; number of lines
 
-    ld      c, a                ; save 17th bit of VRAM addr
+    ex      de, hl
+
+; loop through 16 pixels (8 bytes) of source image
+    ld      b, 8
+
 .loop:
     push    bc
 
-        ld      a, c            ; restore 17th bit of VRAM addr
+        ; for each nibble:
+        ;   if source color == 0 then result = bgcolor
+        ;   else result = source color
 
-        push    hl
-            push    de
-                ld      h, d
-                ld      l, e
-                call    SetVdp_Write
-            pop     de
-        pop    hl
+        ; get byte from source image (2 pixels)
+        ld      a, (de)
+        ld      c, a
 
-        push    hl
-            push    de
-                ld      b, 8            ; bytes per line (16 pixels = 8 bytes on SC5)
-                ld      c, PORT_0        ; you can also write ld bc,#nn9B, which is faster
-                ;ld		hl, ConveyorBelt_Frame1		            ; RAM address (source)
-                
-                ;otir
-.loopOUTI:
-                ld      a, (hl)
-                or      a
-                jp      z, .isTransparentColor
-                outi
-                jp      nz, .loopOUTI
-                jp      .endLoopOUTI
-.isTransparentColor:
-                inc     hl
-                djnz    .loopOUTI
-.endLoopOUTI:
-            pop     de
-        pop    hl
+        ; high nibble (left pixel)
+        and     1111 0000b
+        or      a
+        jp      z, .isTransp
 
-        ld      bc, 8              ; next line of image (16 pixels = 8 bytes on SC5)
-        add     hl, bc
+; source not transp
+        ; IXH = result = source color
+        ld      ixh, a
+        jp      .getLowNibble
 
-        ex      de, hl
-            ld      bc, 128             ; next line of NAMTBL
-            add     hl, bc
-        ex      de, hl
+.isTransp:
+        ; get byte from VRAM NAMTBL (2 pixels)
+        call    BIOS_RDVRM
+
+        ; B = high nibble (left pixel)
+        and     1111 0000b
+        ld      b, a
+        
+        ; IXH = result = bg color
+        ld      ixh, b
+
+.getLowNibble:
+        ; low nibble (left pixel)
+        ld      a, c
+        and     0000 1111b
+        or      a
+        jp      z, .isTransp_Low
+
+; source not transp
+        ; IXH = result = source color
+        ld      b, a
+        ; join with high nibble
+        ld      a, ixh
+        or      b
+        jp      .writeResult
+
+.isTransp_Low:
+        ; get byte from VRAM NAMTBL (2 pixels)
+        call    BIOS_RDVRM
+
+        ; B = high nibble (left pixel)
+        and     0000 1111b
+        ld      b, a
+        
+        ; join with high nibble
+        ld      a, ixh
+        or      b
+
+        ;ld      ixh, a
+
+.writeResult:
+        ; write result to VRAM NAMTBL    
+        ;ld      a, ixh
+        call    BIOS_WRTVRM
+
+        ; next bytes from source and destiny
+        inc     hl
+        inc     de
+
 
     pop     bc
     djnz    .loop
@@ -413,5 +445,5 @@ LoadBackground:
     ret
 
 
-; DataTest:
-;     db  0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11
+TestData:
+    db  0x01, 0x10, 0x11, 0x00, 0xb0, 0xbb, 0x0b, 0xff
