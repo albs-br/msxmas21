6bfd6be9567159a8c2e1d38130c30f8cbf98f730
Andre Baptista <albs_br@yahoo.com.br>
2022-09-24 00:15:17 -0300

    v.0.57.0

--- UpdateAnimations.s
+++ UpdateAnimations.s
@@ -3,214 +3,168 @@ UpdateAnimations:
     ld      a, (BIOS_JIFFY)
     and     0011 1111b
     cp      8
-    jp      z, .frame1
+    jp      z, .frame0
     cp      16
-    jp      z, .frame2
+    jp      z, .frame1
     cp      24
-    jp      z, .frame3
+    jp      z, .frame2
     cp      32
-    jp      z, .frame4
+    jp      z, .frame3
     cp      40
-    jp      z, .frame5
+    jp      z, .frame4
     cp      48
-    jp      z, .frame6
+    jp      z, .frame5
     cp      56
-    jp      z, .frame7
+    jp      z, .frame6
     cp      63
-    jp      z, .frame8
+    jp      z, .frame7
+    ret
+
+.frame0:
+    ; loading frame 0 of snow animation to all 4 parts of left window
+    ld      hl, SNOW_ANIMATION_FRAME_0
+    call    LoadAnimationFrameToAll4Windows
+
+    ; ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
+    ; ; ld      hl, 0
+    ; ; ld      (VdpCommand_SourceX), hl     ; source x
+    ; ; ld      hl, 256
+    ; ; ld      (VdpCommand_SourceY), hl     ; source y
+    ; ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_X
+    ; ; ld      (VdpCommand_DestinyX), hl     ; dest x
+    ; ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_Y
+    ; ; ld      (VdpCommand_DestinyY), hl     ; dest y
+    ; ; ld      hl, VdpCommand 	; execute the copy
+    ; ; call    DoCopy
+
+    ; ld      hl, SNOW_ANIMATION_FRAME_0
+    ; ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
+
+    ; ld      hl, SNOW_ANIMATION_FRAME_0
+    ; ld      b,WINDOW_LEFT_BOTTOM_LEFT_GLASS_X
+    ; ld      c,WINDOW_LEFT_BOTTOM_LEFT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
+
+    ; ld      hl, SNOW_ANIMATION_FRAME_0
+    ; ld      b,WINDOW_LEFT_BOTTOM_RIGHT_GLASS_X
+    ; ld      c,WINDOW_LEFT_BOTTOM_RIGHT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
+
     ret
 
 .frame1:
-    ; test loading frame 0 of snow animation to top left glass of left window
-    ld      hl, 0
-    ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    call    LoadAnimationFrameToWindow
-    ; ld      hl, 0
-    ; ld      (VdpCommand_SourceX), hl     ; source x
-    ; ld      hl, 256
-    ; ld      (VdpCommand_SourceY), hl     ; source y
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ; ld      (VdpCommand_DestinyX), hl     ; dest x
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    ; ld      (VdpCommand_DestinyY), hl     ; dest y
-    ; ld      hl, VdpCommand 	; execute the copy
-    ; call    DoCopy
+    ld      hl, SNOW_ANIMATION_FRAME_1
+    call    LoadAnimationFrameToAll4Windows
 
-    ld      hl, 0
-    ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
-    call    LoadAnimationFrameToWindow
+    ; ld      hl, SNOW_ANIMATION_FRAME_1
+    ; ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
+
+    ; ld      hl, SNOW_ANIMATION_FRAME_1
+    ; ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
 
     ret
 
 .frame2:
-    ; test loading frame 1 of snow animation to top left glass of left window
-    ld      hl, 9
-    ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    call    LoadAnimationFrameToWindow
+    ld      hl, SNOW_ANIMATION_FRAME_2
+    call    LoadAnimationFrameToAll4Windows
 
-    ; ld      hl, 9
-    ; ld      (VdpCommand_SourceX), hl     ; source x
-    ; ld      hl, 256
-    ; ld      (VdpCommand_SourceY), hl     ; source y
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ; ld      (VdpCommand_DestinyX), hl     ; dest x
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    ; ld      (VdpCommand_DestinyY), hl     ; dest y
-    ; ld      hl, VdpCommand 	; execute the copy
-    ; call    DoCopy
+    ; ld      hl, SNOW_ANIMATION_FRAME_2
+    ; ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
 
-    ld      hl, 9
-    ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
-    call    LoadAnimationFrameToWindow
+    ; ld      hl, SNOW_ANIMATION_FRAME_2
+    ; ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
 
     ret
 
 .frame3:
-    ; test loading frame 3 of snow animation to top left glass of left window
-    ld      hl, 16
-    ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    call    LoadAnimationFrameToWindow
-    ; ld      hl, 16
-    ; ld      (VdpCommand_SourceX), hl     ; source x
-    ; ld      hl, 256
-    ; ld      (VdpCommand_SourceY), hl     ; source y
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ; ld      (VdpCommand_DestinyX), hl     ; dest x
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    ; ld      (VdpCommand_DestinyY), hl     ; dest y
-    ; ld      hl, VdpCommand 	; execute the copy
-    ; call    DoCopy
+    ld      hl, SNOW_ANIMATION_FRAME_3
+    call    LoadAnimationFrameToAll4Windows
 
-    ld      hl, 16
-    ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
-    call    LoadAnimationFrameToWindow
+    ; ld      hl, SNOW_ANIMATION_FRAME_3
+    ; ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
+
+    ; ld      hl, SNOW_ANIMATION_FRAME_3
+    ; ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
 
     ret
 
 .frame4:
-    ; test loading frame 4 of snow animation to top left glass of left window
-    ld      hl, 16 + 9
-    ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    call    LoadAnimationFrameToWindow
-    ; ld      hl, 16 + 9
-    ; ld      (VdpCommand_SourceX), hl     ; source x
-    ; ld      hl, 256
-    ; ld      (VdpCommand_SourceY), hl     ; source y
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ; ld      (VdpCommand_DestinyX), hl     ; dest x
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    ; ld      (VdpCommand_DestinyY), hl     ; dest y
-    ; ld      hl, VdpCommand 	; execute the copy
-    ; call    DoCopy
+    ld      hl, SNOW_ANIMATION_FRAME_4
+    call    LoadAnimationFrameToAll4Windows
 
-    ld      hl, 16 + 9
-    ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
-    call    LoadAnimationFrameToWindow
+    ; ld      hl, SNOW_ANIMATION_FRAME_4
+    ; ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
+
+    ; ld      hl, SNOW_ANIMATION_FRAME_4
+    ; ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
 
     ret
 
 .frame5:
-    ; test loading frame 5 of snow animation to top left glass of left window
-    ld      hl, 32
-    ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    call    LoadAnimationFrameToWindow
-    ; ld      hl, 32
-    ; ld      (VdpCommand_SourceX), hl     ; source x
-    ; ld      hl, 256
-    ; ld      (VdpCommand_SourceY), hl     ; source y
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ; ld      (VdpCommand_DestinyX), hl     ; dest x
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    ; ld      (VdpCommand_DestinyY), hl     ; dest y
-    ; ld      hl, VdpCommand 	; execute the copy
-    ; call    DoCopy
+    ld      hl, SNOW_ANIMATION_FRAME_5
+    call    LoadAnimationFrameToAll4Windows
 
-    ld      hl, 32
-    ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
-    call    LoadAnimationFrameToWindow
+    ; ld      hl, SNOW_ANIMATION_FRAME_5
+    ; ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
+
+    ; ld      hl, SNOW_ANIMATION_FRAME_5
+    ; ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
 
     ret
 
 .frame6:
-    ; test loading frame 6 of snow animation to top left glass of left window
-    ld      hl, 32 + 9
-    ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    call    LoadAnimationFrameToWindow
-    ; ld      hl, 32 + 9
-    ; ld      (VdpCommand_SourceX), hl     ; source x
-    ; ld      hl, 256
-    ; ld      (VdpCommand_SourceY), hl     ; source y
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ; ld      (VdpCommand_DestinyX), hl     ; dest x
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    ; ld      (VdpCommand_DestinyY), hl     ; dest y
-    ; ld      hl, VdpCommand 	; execute the copy
-    ; call    DoCopy
+    ld      hl, SNOW_ANIMATION_FRAME_6
+    call    LoadAnimationFrameToAll4Windows
 
-    ld      hl, 32 + 9
-    ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
-    call    LoadAnimationFrameToWindow
+    ; ld      hl, SNOW_ANIMATION_FRAME_6
+    ; ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
+
+    ; ld      hl, SNOW_ANIMATION_FRAME_6
+    ; ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
 
     ret
 
 .frame7:
-    ; test loading frame 7 of snow animation to top left glass of left window
-    ld      hl, 48
-    ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    call    LoadAnimationFrameToWindow
-    ; ld      hl, 48
-    ; ld      (VdpCommand_SourceX), hl     ; source x
-    ; ld      hl, 256
-    ; ld      (VdpCommand_SourceY), hl     ; source y
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ; ld      (VdpCommand_DestinyX), hl     ; dest x
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    ; ld      (VdpCommand_DestinyY), hl     ; dest y
-    ; ld      hl, VdpCommand 	; execute the copy
-    ; call    DoCopy
+    ld      hl, SNOW_ANIMATION_FRAME_7
+    call    LoadAnimationFrameToAll4Windows
 
-    ld      hl, 48
-    ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
-    call    LoadAnimationFrameToWindow
+    ; ld      hl, SNOW_ANIMATION_FRAME_7
+    ; ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
+    ; ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
 
-    ret
-
-.frame8:
-    ; test loading frame 8 of snow animation to top left glass of left window
-
-    ld      hl, 48 + 9
-    ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    call    LoadAnimationFrameToWindow
-    ; ld      hl, 48 + 9
-    ; ld      (VdpCommand_SourceX), hl     ; source x
-    ; ld      hl, 256
-    ; ld      (VdpCommand_SourceY), hl     ; source y
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_X
-    ; ld      (VdpCommand_DestinyX), hl     ; dest x
-    ; ld      hl, WINDOW_LEFT_TOP_LEFT_GLASS_Y
-    ; ld      (VdpCommand_DestinyY), hl     ; dest y
-    ; ld      hl, VdpCommand 	; execute the copy
-    ; call    DoCopy
-
-    ld      hl, 48 + 9
-    ld      b, WINDOW_LEFT_TOP_RIGHT_GLASS_X
-    ld      c, WINDOW_LEFT_TOP_RIGHT_GLASS_Y
-    call    LoadAnimationFrameToWindow
+    ; ld      hl, SNOW_ANIMATION_FRAME_7
+    ; ld      b, WINDOW_LEFT_TOP_RIGHT_GLASS_X
+    ; ld      c, WINDOW_LEFT_TOP_RIGHT_GLASS_Y
+    ; call    LoadAnimationFrameToWindow
 
     ret
 
@@ -244,5 +198,33 @@ LoadAnimationFrameToWindow:
     ld      hl, VdpCommand 	; execute the copy
     
     call    DoCopy
+
+    ret
+
+LoadAnimationFrameToAll4Windows:
+    push    hl
+        ;ld      hl, SNOW_ANIMATION_FRAME_0
+        ld      b,WINDOW_LEFT_TOP_LEFT_GLASS_X
+        ld      c,WINDOW_LEFT_TOP_LEFT_GLASS_Y
+        call    LoadAnimationFrameToWindow
+    pop     hl
+
+    push    hl
+        ld      b,WINDOW_LEFT_TOP_RIGHT_GLASS_X
+        ld      c,WINDOW_LEFT_TOP_RIGHT_GLASS_Y
+        call    LoadAnimationFrameToWindow
+    pop     hl
+
+    push    hl
+        ld      b,WINDOW_LEFT_BOTTOM_LEFT_GLASS_X
+        ld      c,WINDOW_LEFT_BOTTOM_LEFT_GLASS_Y
+        call    LoadAnimationFrameToWindow
+    pop     hl
+
+    push    hl
+        ld      b,WINDOW_LEFT_BOTTOM_RIGHT_GLASS_X
+        ld      c,WINDOW_LEFT_BOTTOM_RIGHT_GLASS_Y
+        call    LoadAnimationFrameToWindow
+    pop     hl
 
     ret
